0eb2e4c314d8183bdc78fdac3c154dc2c98bc099
= <=>
2025-09-12 10:19:22 +0300

    Ability to view what people answered on request / custom 404 page

--- blogrepost_project/settings.py
+++ blogrepost_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j%j7_5vo^izo3xcszbk7(cc4!o!t&x$octq9%8bvdno^rc+77*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
